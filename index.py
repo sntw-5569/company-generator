2747064e3922637fe8b10fa9f372e6a34285b123
sntw-5569 <ore.no.yeah@gmail.com>
2018-03-23 16:21:59 +0900

    dev// add practis files

--- index.py
+++ index.py
@@ -1,6 +1,17 @@
 import json
 import datetime
+from flask import Flask, request, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def index_view():
+    return render_template('index.html')
+
+@app.route('/ent')
+def ent_view():
+    title = 'Company Generator'
+    return render_template('ent.html', title=title)
 
 def handler(event, context):
     data = {
@@ -10,3 +21,6 @@ def handler(event, context):
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5005, debug=True)
